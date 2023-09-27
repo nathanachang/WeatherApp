d02663aebe615b715b909bf7488e902d23799570
Nathan Chang <nathanchang7@yahoo.com>
2023-09-27 17:38:50 -0400

    Added Hello World message to view

--- app/src/main/java/com/capstone/weatherapp/MainActivity.kt
+++ app/src/main/java/com/capstone/weatherapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.capstone.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
