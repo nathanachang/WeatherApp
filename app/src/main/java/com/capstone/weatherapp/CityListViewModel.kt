b9476c1d9d758ec124ba25a427a533b3f74e5e89
Nathan Chang <nathanchang7@yahoo.com>
2023-10-02 17:32:20 -0400

    added private and public lists for gets and sets

--- app/src/main/java/com/capstone/weatherapp/CityListViewModel.kt
+++ app/src/main/java/com/capstone/weatherapp/CityListViewModel.kt
@@ -3,5 +3,6 @@ package com.capstone.weatherapp
 import androidx.lifecycle.ViewModel
 
 class CityListViewModel : ViewModel() {
-    var arrayList = ArrayList<Int>(List(100) { it + 1 })
+    private var _list = ArrayList<Int>(List(100) { it + 1 })
+    public val list = _list
 }
