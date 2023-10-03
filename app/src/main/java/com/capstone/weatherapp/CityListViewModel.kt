8d405cbd8148ef89d70280d3e97e1436b54ba567
Nathan Chang <nathanchang7@yahoo.com>
2023-10-03 12:02:31 -0400

    added getter to public arraylist

--- app/src/main/java/com/capstone/weatherapp/CityListViewModel.kt
+++ app/src/main/java/com/capstone/weatherapp/CityListViewModel.kt
@@ -4,5 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class CityListViewModel : ViewModel() {
     private var _list = ArrayList<Int>(List(100) { it + 1 })
-    public val list = _list
+    val list: ArrayList<Int>
+        get() = _list
 }
